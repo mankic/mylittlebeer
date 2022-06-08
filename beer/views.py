5d4067f705b61e3fc8a088ac2fcd643644b5657f
mankic <miic0803@gmail.com>
2022-06-08 19:38:01 +0900

    수정 - 맥주 정보 raing 순으로 나열, 이미지와 데이터 불러오기, 모달창 작업중

--- beer/views.py
+++ beer/views.py
@@ -5,5 +5,4 @@ from .models import Beer
 def rating(request):
     if request.method == 'GET':
         beers = Beer.objects.order_by('-rating')
-        print(beers)
     return render(request, 'beer/rating.html', {'beers': beers})
