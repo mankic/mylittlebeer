e468d2d02133c155bb274c429d261fd699da433e
DongwooKim <kimphysicsman@gmail.com>
2022-06-08 20:31:10 +0900

    추가 - history model 생성

--- history/models.py
+++ history/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user.models import User
+from beer.models import Beer
 
-# Create your models here.
+
+class History(models.Model):
+    class Meta:
+        db_table = "History"
+
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
