309cf577c4bb763188ded6ae96f177108a52a7e3
creamone <gsukim777@gmail.com>
2022-06-09 22:31:08 +0900

    회원가입 수정,삭제 완료

--- user/urls.py
+++ user/urls.py
@@ -6,10 +6,7 @@ urlpatterns = [
     path('sign-in/', views.sign_in_view, name='sign-in'),
     path('logout/', views.logout, name='logout'),
     path('update/', views.update, name='update'),
-    path('update-confirm/', views.update_confirm, name='update-confirm'),
     path('delete/', views.delete, name='delete'),
-    path('delete-confirm/', views.delete_confirm, name='delete-confirm'),
+
 
 ]
-# path('user/', views.user_view, name='user-list'),  # <- 여기에 컴마 주의!
-# path('user_delete/', views.user_delete_view, name='user_delete'),
--- user/views.py
+++ user/views.py
@@ -29,19 +29,21 @@ def sign_up_view(request):
 
         if password != password2:
             # 패스워드가 같지 않다고 알람
-            return render(request, 'user/signup.html', {'error': '패스워드를 확인해주세요!'})
+            return render(request, 'user/signup.html',{'error':'패스워드를 확인해주세요!'})
         else:
             if username == '' or password == '' or email == '':
-                return render(request, 'user/signup.html', {'error': '빈칸을 채워주세요!'})
+                return render(request, 'user/signup.html', {'error': '사용자 이름과 비밀번호는 필수 값입니다!'})
 
             exist_user = get_user_model().objects.filter(username=username)
+            exist_email = get_user_model().objects.filter(email=email)
+
             if exist_user:
-                return render(request, 'user/signup.html',
-                              {'error': '사용자가 존재합니다'})  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
+                return render(request, 'user/signup.html', {'error':'사용자가 존재합니다'})  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
+            elif exist_email:
+                return render(request, 'user/signup.html', {'error':'이메일이 존재합니다'})  # 이메일이 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
             else:
                 User.objects.create_user(username=username, password=password, email=email)
                 return redirect('/sign-in')
-
 
 def sign_in_view(request):
     if request.method == 'POST':
@@ -66,51 +68,45 @@ def sign_in_view(request):
 @login_required
 def logout(request):
     auth.logout(request)
-    return redirect('/')
+    return redirect('/sign-in')
+
 
 
 
 
 
 @login_required()
-def update_confirm(request):
-    if request.method == 'POST':
+def delete(request):
+    if request.method == 'GET':
+        return render(request,'user/user_delete.html')
+    elif request.method == 'POST':
         user = request.user
         password = request.POST.get("password", "")
         if user.check_password(password):
-            return render("/update")
+            auth.logout(request)
+            user.delete()
+            return render(request,"user/signin.html")
         else:
             raise Exception("잘못된 비밀번호를 입력했습니다.")
-            return redirect('/update_confirm')
-
-@login_required()
-def delete_confirm(request):
-    if request.method == 'POST':
-        user = request.user
-        password = request.POST.get("password", "")
-        if user.check_password(password):
-            return render("/delete")
-        else:
-            raise Exception("잘못된 비밀번호를 입력했습니다.")
-            return redirect('/delete_confirm')
+            return redirect('/user')
 
 @login_required()
 def update(request):
-    if request.method == 'POST':
+
+    if request.method == 'GET':
+        return render(request, 'user/user_update.html')
+
+    elif request.method == 'POST':
         user = request.user
-        username = request.POST.get("username,", "")
-        password = request.POST.get("password", "")
+
+
         email = request.POST.get("email", "")
 
-        user.username = username
-        user.password = password
+
+
         user.email = email
         user.save()
-    return render("/")
-@login_required()
-def delete(request):
-    user = request.user
-    user.delete()
-    return redirect('/')
+
+    return redirect('/recommend')
 
 
